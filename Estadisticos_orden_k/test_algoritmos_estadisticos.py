d17bcd20a177bea47696ccb7955d3f4d3f42b0f6
LucasLudueno <luks93.racing@gmail.com>
2016-10-02 18:08:07 -0300

    k_heap_select

--- Estadisticos_orden_k/test_algoritmos_estadisticos.py
+++ Estadisticos_orden_k/test_algoritmos_estadisticos.py
@@ -2,6 +2,8 @@
 from k_selecciones import k_selecciones
 from fuerza_bruta import fuerza_bruta
 from ordenar_seleccionar import ordenar_seleccionar
+from k_heap_select import k_heap_select
+from MinHeap import MinHeap
 
 import unittest
 
@@ -18,6 +20,12 @@ class TestEstadisticoClass(unittest.TestCase):
 
 	def test_dado_un_conjunto_ordenar_seleccionar_devuelve_el_k_elem_mas_chico(self):
 		self.assertEqual(ordenar_seleccionar(conjunto, k), 22)
+
+	def test_dado_un_conjunto_k_heap_select_devuelve_el_k_elem_mas_chico(self):
+		heap = MinHeap()
+		for elem in conjunto:
+			heap.encolar(elem)
+		self.assertEqual(k_heap_select(heap, k), 22)
 		
 if __name__ == '__main__':
     unittest.main()
