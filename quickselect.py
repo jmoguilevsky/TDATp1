3365dfbb3c36f0f8aa6fa07e149777c4c78ca0e7
diegofk26 <diegofk26@gmail.com>
2016-10-02 21:01:19 -0300

    Subo el algoritmo de quickselect - #6
    con el tambien se agrego su correspondiente prueba en
    test_algoritmos_estadisticos.py

---
+++ quickselect.py
@@ -0,0 +1,33 @@
+def swap(a,b):
+    return b,a
+
+
+def partition(list, left,right):
+    storeIndex = left
+    pivotValue = list[right]
+
+    for i in range(left,right):
+        if( list[i] < pivotValue ):
+            list[i],list[storeIndex] = swap(list[i] , list[storeIndex])
+            storeIndex = storeIndex + 1
+
+    list[right],list[storeIndex] = swap(list[right],list[storeIndex])
+    return storeIndex
+
+
+
+def quickselect(list,left,right,k):
+
+    listRelativeK = k - 1;
+    if ( left == right ):
+        return list[left]
+
+    pivotIndex = partition(list,left,right)
+
+    if listRelativeK == pivotIndex:
+        return list[listRelativeK]
+    else:
+        if listRelativeK < pivotIndex:
+             return quickselect(list,left,pivotIndex - 1,k)
+        else:
+            return quickselect(list,pivotIndex + 1, right, k)
--- test_algoritmos_estadisticos.py
+++ test_algoritmos_estadisticos.py
@@ -2,6 +2,7 @@
 from k_selecciones import k_selecciones
 from fuerza_bruta import fuerza_bruta
 from ordenar_seleccionar import ordenar_seleccionar
+from quickselect import quickselect
 
 import unittest
 
@@ -9,7 +10,7 @@ conjunto = [1,6,9,3,5,10,3,12,15,60,0,9,4,41,22,16]
 k = 14
 
 class TestEstadisticoClass(unittest.TestCase):
-	
+
 	def test_dado_un_conjunto_k_selecciones_devuelve_el_k_elem_mas_chico(self):
 		self.assertEqual(k_selecciones(conjunto, k), 22)
 
@@ -18,7 +19,9 @@ class TestEstadisticoClass(unittest.TestCase):
 
 	def test_dado_un_conjunto_ordenar_seleccionar_devuelve_el_k_elem_mas_chico(self):
 		self.assertEqual(ordenar_seleccionar(conjunto, k), 22)
-		
+
+	def test_dado_un_conjunto_quickselect_devuelve_el_k_elem_mas_chico(self):
+		self.assertEqual(quickselect(conjunto,0,len(conjunto) - 1,k),22)
+
 if __name__ == '__main__':
     unittest.main()
-
